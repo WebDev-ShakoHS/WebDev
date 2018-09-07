6fe8c0f8ec43d5c00f5a42655acd5768278f32b6
rmainhar <rmainhar@shakopee.k12.mn.us>
2018-09-07 14:26:31 -0500

    Deleted 2017-18 work

--- Version5.0/Webdevuser15.js
+++
@@ -1,9 +0,0 @@
-function changeText()  {
-    document.getElementById("TextChange").innerHTML=Thanks for liking my Webpage";
-    }
-    
-    var space= " ";
-    var pos=o;
-    var msg= "User 15";
-    
-    
