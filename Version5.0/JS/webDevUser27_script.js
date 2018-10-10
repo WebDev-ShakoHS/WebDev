54697b3ad735d73a2c91f74ae97af70f89614344
238211 <238211@shakopeeschools.org>
2018-10-10 16:55:45 -0500

    More upDates
    Version5.0 fixed the title text to scroll again.

--- Version5.0/JS/webDevUser27_script.js
+++ Version5.0/JS/webDevUser27_script.js
@@ -7,9 +7,9 @@ var msg = "User 27";
 
 function Scroll() {
     document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
-}
-pos++;
-if (pos > msg.length) {
-window.setTimeout("Scroll()", 0);
+
+    pos++;
+    if (pos > msg.length) pos = 0;
+    window.setTimeout("Scroll()", 0);
 }
 Scroll();
