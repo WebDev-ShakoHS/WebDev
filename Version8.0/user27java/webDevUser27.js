9729f003b313eecb22b8af8a32bafd92eb58db65
238211 <238211@shakopeeschools.org>
2019-01-16 15:06:22 -0600

    V.8
    code and sql

--- Version8.0/user27java/webDevUser27.js
+++ Version8.0/user27java/webDevUser27.js
@@ -41,3 +41,20 @@
     }
     
 })(jQuery);
+
+function changeText() {
+    document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+}
+
+var space = " ";
+var pos = 0;
+var msg = "OOooOOFFf";
+
+function Scroll() {
+    document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+
+pos++;
+if (pos > msg.length) pos = 0;
+window.setTimeout("Scroll()", 0);
+}
+Scroll();
--- Version8.0/user27java/webDevUser27_paragraph.js
+++ Version8.0/user27java/webDevUser27_paragraph.js
@@ -55,3 +55,17 @@
                 }); 
                 
             })();
+
+
+var space = " ";
+var pos = 0;
+var msg = "Coolsvile";
+
+function Scroll() {
+    document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+
+pos++;
+if (pos > msg.length) pos = 0;
+window.setTimeout("Scroll()", 0);
+}
+Scroll();
