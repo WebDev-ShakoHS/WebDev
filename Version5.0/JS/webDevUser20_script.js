fa66e37a27416268641d5ff49eb365812865ff4c
KiTsegaye <46534786+KiTsegaye@users.noreply.github.com>
2019-09-14 14:24:37 -0500

    changes for versions 3 and 4

--- Version5.0/JS/webDevUser20_script.js
+++
@@ -1,8 +0,0 @@
-function changeText() {
- document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
-    document.body.style.background = "#FF8F00"
-}
-function coco() {
-    document.getElementById('f').play();
-}
-
