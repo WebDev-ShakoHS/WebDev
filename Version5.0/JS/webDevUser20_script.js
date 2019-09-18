8f3b9f00be8be13c8269da16320d55bcee2336db
KidusT <235792@shakopeeschools.org>
2019-09-18 08:21:39 -0500

    buttons added

--- Version5.0/JS/webDevUser20_script.js
+++ Version5.0/JS/webDevUser20_script.js
@@ -1,6 +1,6 @@
 function changeText() {
  document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
-    document.body.style.background = "#FF8F00"
+
 }
 function coco() {
     document.getElementById('f').play();
