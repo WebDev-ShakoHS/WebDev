e960c523f8dd73f8da5ef61e6026d5fdf7beba56
David Tesar <309932@shakopeeschools.org>
2019-11-06 09:44:17 -0600

    Final Website

---
+++ Version9.0/final/crud/index.php
@@ -0,0 +1,144 @@
+<?php
+
+require_once ("../crud/php/component.php");
+require_once ("../crud/php/operation.php");
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GAMESTORE</title>
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="../webDevUser17.html">HOME</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="../webDevUser17_aboutme.html">ABOUT ME</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../contact.html">CONTACT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../FAQ/webDevUser17_FAQ.html">FAQ</a>
+      </li>    
+      <li class="nav-item">
+        <a class="nav-link" href="../movies/index.html">MOVIES</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../crud/">GAMESTORE</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../weatherjs/index.html">WEATHER</a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="../games.html">GAMES</a>
+      </li>    
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>    
+    
+    
+<main>
+    <div class="container text-center">
+        <h1 class="py-4 bg-dark text-light rounded">Tell me game that you want! <i class="fas fa-dice"></i></h1>
+
+        <div class="d-flex justify-content-center">
+            <form action="" method="post" class="w-50">
+                <div class="pt-2">
+                    <?php inputElement("<i class='fas fa-id-badge'></i>","ID", "game_id",setID()); ?>
+                </div>
+                <div class="pt-2">
+                    <?php inputElement("<i class='fas fa-gamepad'></i>","Game Name", "game_name",""); ?>
+                </div>
+                <div class="row pt-2">
+                    <div class="col">
+                        <?php inputElement("<i class='fas fa-people-carry'></i>","Publisher", "game_publisher",""); ?>
+                    </div>
+                    <div class="col">
+                        <?php inputElement("<i class='fas fa-dollar-sign'></i>","Price", "game_price",""); ?>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                        <?php buttonElement("btn-create","btn btn-success","<i class='fas fa-plus'></i>","create","data-toggle='tooltip' data-placement='bottom' title='Create'"); ?>
+                        <?php buttonElement("btn-read","btn btn-primary","<i class='fas fa-sync'></i>","read","data-toggle='tooltip' data-placement='bottom' title='Read'"); ?>
+                        <?php buttonElement("btn-update","btn btn-light border","<i class='fas fa-pen-alt'></i>","update","data-toggle='tooltip' data-placement='bottom' title='Update'"); ?>
+                        <?php buttonElement("btn-delete","btn btn-danger","<i class='fas fa-trash-alt'></i>","delete","data-toggle='tooltip' data-placement='bottom' title='Delete'"); ?>
+                        <?php deleteBtn();?>
+                </div>
+            </form>
+        </div>
+
+        <!-- Bootstrap table  -->
+        <div class="d-flex table-data">
+            <table class="table table-striped table-dark">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>ID</th>
+                        <th>Game Name</th>
+                        <th>Publisher</th>
+                        <th>Price</th>
+                        <th>Edit</th>
+                    </tr>
+                </thead>
+                <tbody id="tbody">
+                   <?php
+
+
+                   if(isset($_POST['read'])){
+                       $result = getData();
+
+                       if($result){
+
+                           while ($row = mysqli_fetch_assoc($result)){ ?>
+
+                               <tr>
+                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['id']; ?></td>
+                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['game_name']; ?></td>
+                                   <td data-id="<?php echo $row['id']; ?>"><?php echo $row['game_publisher']; ?></td>
+                                   <td data-id="<?php echo $row['id']; ?>"><?php echo '$' . $row['game_price']; ?></td>
+                                   <td ><i class="fas fa-edit btnedit" data-id="<?php echo $row['id']; ?>"></i></td>
+                               </tr>
+
+                   <?php
+                           }
+
+                       }
+                   }
+
+
+                   ?>
+                </tbody>
+            </table>
+        </div>
+
+
+    </div>
+</main>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script src="php/main.js"></script>
+</body>
+</html>
---
+++ Version9.0/final/mail.php
@@ -0,0 +1,11 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+$content="From: $name \n Email: $email \n Message: $message";
+$recipient = "david2tesar@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Email sent!";
+?>
