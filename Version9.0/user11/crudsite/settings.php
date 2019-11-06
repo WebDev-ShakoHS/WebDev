0b352ca5befb9e2caaac458db0cc0caade9e5010
boostedape <54854144+boostedape@users.noreply.github.com>
2019-11-06 08:40:39 -0600

    Final Version

---
+++ Version9.0/user11/crudsite/settings.php
@@ -0,0 +1,14 @@
+<html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css"> 
+
+<p>
+    <a href="Welcome.php" class="btn btn-danger">Back</a>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        
+<a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+
+<img src="tyisgay.jpeg">    
+    </p>
+
+
+</html>
