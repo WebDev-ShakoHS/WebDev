d6b71f76a8aed481f49d994aa71f0268e7bd9077
GDueffert <57813360+GDueffert@users.noreply.github.com>
2020-02-11 13:24:40 -0600

    JAVA color change

---
+++ Version5.0/user08/JS/SampleJS.js
@@ -0,0 +1,24 @@
+function changeText() {
+    document.getElementById("textChange").innerHTML = "Thanks, now you wont die";
+}
+
+
+function changeGreen() {
+    document.getElementById("textChange").style.color = "#00FF00";
+}
+
+function changeRed() {
+    document.getElementById("textChange").style.color = "#FF0000";
+}
+
+function changeBlue() {
+    document.getElementById("textChange").style.color = "#0000FF";
+}
+
+function changeBlack() {
+    document.getElementById("textChange").style.color = "#000000";
+}
+
+function BGBlack() {
+  document.body.style.backgroundColor = "#000000";
+}
