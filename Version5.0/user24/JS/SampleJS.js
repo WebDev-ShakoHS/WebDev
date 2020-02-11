716db88d1a2752f78c6ef2edbbac46f214a51ce1
Vince Resch <235329@shakopeeschools.org>
2020-02-11 13:25:04 -0600

    background color change

---
+++ Version5.0/user24/JS/SampleJS.js
@@ -0,0 +1,7 @@
+function changeText() {
+ document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
+}
+
+function Lime() {
+    document.body.style.backgroundColor = "#bfff00";
+}
