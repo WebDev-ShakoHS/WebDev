7efddd6d02777ea02bd66172e954cdf351679f02
PeterAmendt <225142@shakopeeschools.org>
2020-02-18 12:59:40 -0600

    2.2 JavaScript Added More Features

--- Version5.0/user01/JS/SampleJS.js
+++ Version5.0/user01/JS/SampleJS.js
@@ -1,15 +1,19 @@
-var space = " ";
-var pos = 0;
-var count = 0;
-var ccpc = 1;
-var msg = "User 1";
-add1cpost = 25;
+space = " ";
+pos = 0;
+count = 0;
+ccpc = 1;
+msg = "User 1";
 
 function uptick() {
     var uotput = document.getElementById("counter");
-    count + ccpc;
+    count++;
+    uotput.style.fontSize='25px';
     uotput.innerHTML = count;
-
+    setInterval(norrmalizetext(), 100);
+}
+function norrmalizetext() {
+    var uotput = document.getElementById("counter");
+    uotput.style.fontSize = '20px';
 }
 function findmouse() {
     document.getElementById("xychords").innerHTML = "X= " + event.clientX + "<br>Y= " + event.clientY;
@@ -28,23 +32,18 @@ function off() {
 function annoy() {
     alert("hi");
 }
+
 function cpccost() {
     document.getElementById("cpc").innerHTML = cost;
 }
-function cpcu() {
-    if (cost <= count){
-        ccpc +=1;
-        count - cost;
-        cost + 20;
-    }
-}
+
 function blurred() {
     var x = document.getElementById("inputter");
     x.value = x.value.toUpperCase();
 }
 
 function changeText() {
-    document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+    document.getElementById("textChange").innerHTML = "This is a Cookie";
     var x = document.getElementById("textChange");
     x.style.fontSize = "25px";
     x.style.color = "red";
@@ -59,5 +58,3 @@ function Scroll() {
     window.setTimeout("Scroll()", 0);
 }
 Scroll();
-
-
