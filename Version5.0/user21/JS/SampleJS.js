3954f76d68ee52d50f7425caf473e9b6ebd2cd25
RIley-coder <60401358+RIley-coder@users.noreply.github.com>
2020-02-18 13:25:03 -0600

    Riley-coder

--- Version5.0/user21/JS/SampleJS.js
+++ Version5.0/user21/JS/SampleJS.js
@@ -6,11 +6,43 @@ var space = " ";
 var pos = 0;
 var msg = "WebDev User #21";
 
-function Scroll(){
-document.title = msg.substring(pos, msg.length) + space +msg.substring(0,pos);
+function Scroll() {
+    document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
 
-pos++;
-if (pos > msg.length) pos = 0;
-window.setTimeout("Scroll()", 0);
+    pos++;
+    if (pos > msg.length) pos = 0;
+    window.setTimeout("Scroll()", 0);
 }
 Scroll();
+
+var space = " ";
+var pos = 0;
+var msg = "WebDev user #21";
+
+function Scroll() {
+    document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+
+    pos++;
+    if (pos > msg.length) pos = 0;
+    window.setTimeout("Scroll()", 0);
+}
+Scroll();
+
+
+
+function allCaps() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Gotta love food";
+}
+
+function annoyingAlert() {
+    alert("You pressed a key inside the input field");
+}
+
+function changeParagraph() {
+    document.getElementById("changeclick").innerHTML = "Hello World";
+}
