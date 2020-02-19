ff756a00db9d189617a422eb2810d56e53bc9660
Collin SHS <33844020+231603@users.noreply.github.com>
2020-02-19 11:29:35 -0600

    Changed V5, added stuff

--- Version5.0/user11/SampleJS.js
+++ Version5.0/user11/SampleJS.js
@@ -29,3 +29,16 @@ var stupidButton = {
 };
 
 stupidButton.init();
+
+var space = " ";
+var pos = 0;
+var msg = "User 19";
+
+function Scroll(){
+document.title = msg.substring(pos, msg.length) + space +msg.substring(0,pos);
+
+pos++;
+if (pos > msg.length) pos = 0;
+window.setTimeout("Scroll()", 0);
+}
+Scroll();
