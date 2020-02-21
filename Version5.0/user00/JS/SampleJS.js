6f8fddd2a912703ae807f95643f726faf8d35a0f
Mr. Mainhardt <rmainhar@shakopee.k12.mn.us>
2020-02-21 11:18:07 -0600

    Version 5

--- Version5.0/user00/JS/SampleJS.js
+++ Version5.0/user00/JS/SampleJS.js
@@ -1,4 +1,8 @@
 function changeText() {
- document.getElementById("textChange").innerHTML="<div class='isiah'> Thanks for liking my Webpage</div>";
+    document.getElementById("textChange").innerHTML = "<div class='isiah'> Thanks for liking my Webpage</div>";
 }
 
+function allCaps() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
