0aa340de3dae8a571af55a053ec586aeff460b8a
303939x <303939@shakopeeschools.org>
2020-02-26 18:50:40 -0600

    changes to a lot of things

--- Version5.0/user32/JS/SampleJS.js
+++ Version5.0/user32/JS/SampleJS.js
@@ -13,3 +13,23 @@
       }
       Scroll();
   }
+
+  function allcaps() {
+
+      var x = document.getElementById("fname");
+      x.value = x.value.toUpperCase();
+  }
+
+  function changetext() {
+
+      document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+  }
+
+  function Scroll() {
+
+      document.title = msg.substring(pos, msg.lenght) + space + msg.substring(0, pos);
+      pos++;
+      if (pos > msg.length) pos = 0;
+      window.setTimeout("Scroll()", 0);
+  }
+  Scroll();
