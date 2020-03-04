384251d84f6099c925889d9883c51709f9e1e703
226402 <226402@shakopeeschools.org>
2020-03-04 12:54:48 -0600

    Update index.php

--- Version8.0/user02/weather/index.php
+++ Version8.0/user02/weather/index.php
@@ -1,7 +1,7 @@
 <?php
 $apiKey = "API KEY"; //You will need to add in the 
 $cityId = "5046997"; //5046997 Shakopee City Id
-$units = "metric";//metric-Celcius  imperial-Farhenheit
+$units = "metric";//imperial-Farhenheit
 if ($units == 'metric'){//Changes the $temp varaible to match 
     $temp = "C";
 }
