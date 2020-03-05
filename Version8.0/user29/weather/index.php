c3241e8a73045625dbb60cf47783a481a1b19512
Brian Vo <310175@c1mrx7hzh3qd.isd720.com>
2020-03-05 12:36:44 -0600

    Update index.php

--- Version8.0/user29/weather/index.php
+++ Version8.0/user29/weather/index.php
@@ -1,5 +1,5 @@
 <?php
-$apiKey = "API KEY"; //You will need to add in the 
+$apiKey = "4c60d01599f00520363ed76e87d2aebd"; //You will need to add in the 
 $cityId = "5046997"; //5046997 Shakopee City Id
 $units = "metric";//metric-Celcius  imperial-Farhenheit
 if ($units == 'metric'){//Changes the $temp varaible to match 
