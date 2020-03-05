316e338ca54bb6a9ad809bdbcf1c0fd32af47576
Zillasafe <60361771+Zillasafe@users.noreply.github.com>
2020-03-05 13:23:10 -0600

    oh jeebus thats a lot
    I'm trying to commit v.8 but I guess theres still a lot from v.7

---
+++ Version7.0/user03/JS/SampleJS.js
@@ -0,0 +1,63 @@
+function whitefacePic() {
+    document.getElementById("ClownPic").innerHTML = "<img src='images/Whiteface.jpg' width='400'>";
+}
+
+function augustePic() {
+    document.getElementById("ClownPic").innerHTML = '<img src="images/Auguste.jpg" width="450">';
+}
+
+function trampPic() {
+    document.getElementById("ClownPic").innerHTML = '<img src="images/Tramp.jpg" width="280">';
+}
+
+function rodeoPic() {
+    document.getElementById("ClownPic").innerHTML = '<img src="images/Rodeo.jpg" width="300">';
+}
+
+function mimePic() {
+    document.getElementById("ClownPic").innerHTML = '<img src="images/Mime.jpg" width="400">';
+}
+
+function creepyPic() {
+    document.getElementById("ClownPic").innerHTML = '<img src="images/Creepy.jpg" width="500">';
+}
+
+function jesterPic() {
+    document.getElementById("ClownPic").innerHTML = '<img src="images/Jester.jpg" width="250">';
+}
+
+function harlequinPic() {
+    document.getElementById("ClownPic").innerHTML = '<img src="images/Harlequin.jpeg" width="300">';
+}
+
+function whitefaceInfo() {
+    document.getElementById("ClownInfo").innerHTML = "wow"
+}
+
+function augusteInfo() {
+    document.getElementById("ClownInfo").innerHTML = "oh my"
+}
+
+function trampInfo() {
+    document.getElementById("ClownInfo").innerHTML = "heck"
+}
+
+function rodeoInfo() {
+    document.getElementById("ClownInfo").innerHTML = "woah"
+}
+
+function mimeInfo() {
+    document.getElementById("ClownInfo").innerHTML = "oof"
+}
+
+function creepyInfo() {
+    document.getElementById("ClownInfo").innerHTML = "ack!"
+}
+
+function jesterInfo() {
+    document.getElementById("ClownInfo").innerHTML = "noice"
+}
+
+function harlequinInfo() {
+    document.getElementById("ClownInfo").innerHTML = "squeak"
+}
