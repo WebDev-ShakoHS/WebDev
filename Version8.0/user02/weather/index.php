929aceebd23cb2376b63eff9e6a49ee2903d5c04
226402 <226402@shakopeeschools.org>
2020-03-05 12:22:23 -0600

    Update index.php

--- Version8.0/user02/weather/index.php
+++ Version8.0/user02/weather/index.php
@@ -21,9 +21,6 @@ curl_setopt($ch, CURLOPT_VERBOSE, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $response = curl_exec($ch);
 
-
-test
-
 curl_close($ch);
 $data = json_decode($response);
 $currentTime = time();
