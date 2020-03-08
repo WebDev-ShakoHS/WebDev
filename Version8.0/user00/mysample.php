6986c99e33e0bc149a88eecaca7966676e4816ca
Mainhardt <rmainhar@shakopee.k12.mn.us>
2020-03-07 22:45:46 -0600

    Adde javaScript weather

--- Version8.0/user00/mysample.php
+++ Version8.0/user00/mysample.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL);  // Turn on all errors, warnings and notices for easier 
 // API request variables
 $endpoint = 'http://svcs.ebay.com/services/search/FindingService/v1';  // URL to call
 $version = '1.0.0';  // API version supported by your application
-$appid = 'KEY HERE';  // Replace with your own AppID
+$appid = 'RobertMa-Shakopee-PRD-169ec6b8e-bb30ba02';  // Replace with your own AppID
 $globalid = 'EBAY-US';  // Global ID of the eBay site you want to search (e.g., EBAY-DE)
 $query = 'screwdrivers';  // You may want to supply your own query
 $safequery = urlencode($query);  // Make the query URL-friendly
