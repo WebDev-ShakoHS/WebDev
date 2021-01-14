4480a927422d4df4cfef5b738a967db76f28fc84
aheedatif <228949@shakopeeschools.org>
2021-01-14 12:51:03 -0600

    Added JAVA

--- Version5.0/user03/JS/SampleJS.js
+++ Version5.0/user03/JS/SampleJS.js
@@ -1,28 +1,45 @@
-const newLocal = "Thanks for liking";
+const newLocal = "Thanks!";
 function changeText() {
-    document.getElementById("textChange").innerHTML= "Thanks!"
-    }
+    document.getElementById("textChange").innerHTML = "Thanks!"
+}
 
-    function preferedBrowser() {
-      prefer = document.forms[0].browsers.value;
-      alert(`You prefer browsing internet with ${prefer}`);
+function preferedBrowser() {
+    prefer = document.forms[0].browsers.value;
+    alert(`You prefer browsing internet with ${prefer}`);
+}
 
-      function confirmInput() {
-        fname = document.forms[0].fname.value;
-        alert("Hello " + fname + "! You will now be redirected to www.w3Schools.com");
-      }
 
-      function writeMessage() {
-        document.forms[0].mySecondInput.value = document.forms[0].myInput.value;
-      }
+function confirmInput() {
+    fname = document.forms[0].fname.value;
+    alert("Hello " + fname + "! You will now be redirected to www.w3Schools.com");
+}
 
-      
-      } 
+function writeMessage() {
+    document.forms[0].mySecondInput.value = document.forms[0].myInput.value;
+}
 
-      function getEventType(event) { 
-        document.getElementById("demo").innerHTML = event.type;
-      }
 
-      function message() {
-        alert("This alert box was triggered by the onreset event handler");
-      }
+
+
+function getEventType(event) {
+    document.getElementById("demo").innerHTML = event.type;
+}
+
+function message() {
+    alert("This alert box was triggered by the onreset event handler");
+}
+
+function Capitalize() {
+    var x = document.getElementById("Uppercase");
+    x.value = x.value.toUpperCase();
+}
+
+function changebackground(x) {
+    x.style.background = "green";
+  }
+
+  function getEventType(event) { 
+    document.getElementById("Eventpopup").innerHTML = event.type;
+  }
+
+
