2fc5b3cce4828030db9143649a4564ed2bab6f43
Goomy64 <hayden.bock@outlook.com>
2021-01-25 13:45:48 -0600

    Added some stuff to the javascript function

--- Version7.0/user07/JS/SampleJS.js
+++ Version7.0/user07/JS/SampleJS.js
@@ -3,14 +3,14 @@ function randomMusic() {
 
     if(number === 0)
     {
-        document.getElementById("songName").innerHTML = "First message.";
+        document.getElementById("songName").innerHTML = "Panzerkampf, By Sabaton. (This song is about the Battle of Kursk)";
     }
     else if(number === 1)
     {
-        document.getElementById("songName").innerHTML = "Second message.";
+        document.getElementById("songName").innerHTML = "The Only Thing They Fear Is You, By Mick Gordon.";
     }
     else
     {
-        document.getElementById("songName").innerHTML = "Third message.";
+        document.getElementById("songName").innerHTML = "Light In The Black, By Sabaton. (This song is about peacekeeping forces, ex. The United Nations) ";
     }
 }
