6338015927d98196f6beaa55206fde4d7f295e48
311075 <74618967+311075@users.noreply.github.com>
2021-01-28 11:39:46 -0600

    FINAL STUFF

--- Version5.0/user25/JS/SampleJS.js
+++ Version5.0/user25/JS/SampleJS.js
@@ -1,34 +1,28 @@
 function changeText() {
-    document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
-    }
+  document.getElementById("textChange").innerHTML = "Thanks for liking my Webpage";
+}
 
-    function myFunction() {
-        var x = document.getElementById("fname");
-        x.value = x.value.toUpperCase();
-      }
+function myFunction() {
+  var x = document.getElementById("fname");
+  x.value = x.value.toUpperCase();
+}
 
-      function myFunction(x) {
-        x.style.background = "Purple";
-      }
 
-      function myFunction() {
-        document.getElementById("demo").innerHTML = "yeeee";
-      }
-      function writeMessage() {
-        document.forms[0].mySecondInput.value = document.forms[0].myInput.value;
-      }
 
-      function loadImage() {
-        alert("Image is loaded");
-      }
 
-      function message() {
-        alert("This alert box was triggered by the onreset event handler");
-      }
-      function isKeyPressed(event) {
-        var text = "The shift key was NOT pressed!";
-        if (event.shiftKey == 1) {
-          text = "The shift key was pressed!";
-        }
-        document.getElementById("demo").innerHTML = text;
-      }
+function writeMessage() {
+  document.forms[0].mySecondInput.value = document.forms[0].myInput.value;
+}
+
+
+function confirmInput() {
+  fname = document.forms[0].fname.value;
+  alert("Hello " + fname + "! You will now be redirected to www.w3Schools.com");
+}
+function myFunction(elmnt, clr) {
+  elmnt.style.color = clr;
+}
+function myFunction(x) {
+  x.style.background = "red";
+}
+
