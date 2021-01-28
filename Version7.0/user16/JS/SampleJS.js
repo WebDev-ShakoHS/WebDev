342d12b13c3cfb6d0146f54c0e34e69efd8914eb
Tyler02886 <226754@shakopeeschools.org>
2021-01-28 12:22:57 -0600

    Version 7
    Final Website

---
+++ Version7.0/user16/JS/SampleJS.js
@@ -0,0 +1,9 @@
+function confirmInput() {
+    fname = document.forms[0].fname.value;
+    alert("Hello " + fname + "! You will now be redirected to www.Youtube.com");
+}
+
+function Functionkeypress() {
+    alert("You pressed a key inside the input field");
+}
+
