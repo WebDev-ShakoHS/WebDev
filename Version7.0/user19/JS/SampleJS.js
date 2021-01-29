dbed744f11265f9c214c5220d83b50d4fc2308c5
218christian218 <74561522+218christian218@users.noreply.github.com>
2021-01-28 23:35:59 -0600

    Update SampleJS.js

---
+++ Version7.0/user19/JS/SampleJS.js
@@ -0,0 +1,10 @@
+function changeFont1() {
+  document.textContent.style.changeFont() = "Times New Roman"
+}
+function changeText() {
+  document.getElementById("textChange").innerHTML = "Thank you for viewing my website!";
+}
+
+var space = " ";
+var pos = 0;
+var msg = "User 19";
