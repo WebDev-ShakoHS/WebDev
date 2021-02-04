42860f99a7923c0836f523ef7c9464ef5771397e
GavanAvery <233869@shakopeeschools.org>
2021-02-04 10:42:23 -0600

    Version5.0C2
    Just made a few more little changes

---
+++ Version5.0/user04/JS/SampleJS.js
@@ -0,0 +1,15 @@
+function changeText() {
+    document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
+   }
+   var space = " ";
+var pos = 0;
+var msg = "User 19";
+
+function Scroll(){
+document.title = msg.substring(pos, msg.length) + space +msg.substring(0,pos);
+
+pos++;
+if (pos > msg.length) pos = 0;
+window.setTimeout("Scroll()", 0);
+}
+Scroll();
