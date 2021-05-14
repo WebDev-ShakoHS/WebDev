ddf59ffe2cf6fdc01db6a06cdd3e0f34e6405ae0
AlexNiedan <82393057+AlexNiedan@users.noreply.github.com>
2021-05-14 08:22:17 -0500

    version 6
    completed version 6

---
+++ Version6.0/user16/JS/script.js
@@ -0,0 +1,15 @@
+function changeText() {
+    document.getElementById("textChange").innerHTML="<u>Thanks for liking my Webpage</u>";
+   }
+function hidetext() {
+    document.getElementById("hide-text").style.display='none'; 
+}
+
+function openWin() {
+    window.open("https://www.google.com/?safe=active&ssui=on");
+}
+
+function alertbox() {
+    alert("I am an alert box!");
+  }
+  
