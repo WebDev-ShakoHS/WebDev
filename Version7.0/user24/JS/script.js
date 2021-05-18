ee632abf83c4ceaff0f17319914b36c97d280cb3
EvanT124 <225208@shakopeeschools.org>
2021-05-18 13:58:13 -0500

    Start of Version 7w
    Added a nav bar, a jumbotron a banner, and cards.

---
+++ Version7.0/user24/JS/script.js
@@ -0,0 +1,21 @@
+function openwar() {
+window.open("https://www.pcgamesn.com/war-thunder/war-thunder-beginners-guide-tips-and-tricks-for-air-battles")
+}
+
+
+
+function openrust () {
+    window.open("https://www.rustafied.com/rust-guides ")
+
+
+}
+
+function openr6() {
+    window.open("https://guides.gamepressure.com/tom-clancys-rainbow-six-siege/ ")
+
+}
+
+function openD2() {
+    window.open("https://www.shacknews.com/article/101225/destiny-2-complete-strategy-guide")
+
+}
