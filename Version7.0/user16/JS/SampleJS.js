909a640bb116a6c252abffa2f36bf44930cd9f1b
AlexNiedan <82393057+AlexNiedan@users.noreply.github.com>
2021-05-19 09:40:07 -0500

    Wednesday

--- Version7.0/user16/JS/SampleJS.js
+++ Version7.0/user16/JS/SampleJS.js
@@ -14,3 +14,23 @@ function openCity(evt, cityName) {
   
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
+  
+  function myFunction() {
+    var btn = document.createElement("BUTTON");
+    document.body.appendChild(btn);
+  }
+  var el_up = document.getElementById("GFG_UP");
+  var el_down = document.getElementById("GFG_DOWN");
+  var str = "Click on button to change the background color";
+
+  el_up.innerHTML = str;
+
+  function changeColor(color) {
+      document.body.style.background = color;
+  }
+    
+  function gfg_Run() {
+      changeColor('yellow');
+      el_down.innerHTML = "Background Color changed";
+  }        
+  
