f90fa4874ab85719222160ed617298e43ac48c94
LandonVaupel <82455477+LandonVaupel@users.noreply.github.com>
2021-05-20 12:36:13 -0500

    updated 1.7

---
+++ Version7.0/user26/JS/script.js
@@ -0,0 +1,23 @@
+function ButtonClick() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+function changeText() {
+    document.getElementById("textChange").innerHTML = "<b> Click on the boxes to add to your order</b>";
+}
+function TextAlert() {
+    alert("Successful ID");
+}
