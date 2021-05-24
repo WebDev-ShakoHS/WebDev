a7e7d3b8598534c42de1347063c4ba1912f57ea3
ColeTruax <82454839+ColeTruax@users.noreply.github.com>
2021-05-24 09:15:03 -0500

    Website

--- Version7.0/user25/JS/script.js
+++ Version7.0/user25/JS/script.js
@@ -1,3 +1,17 @@
 function changeText() {
     document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
    }
+   function zoomIn(event) {
+    var element = document.getElementById("overlay");
+    element.style.display = "inline-block";
+    var img = document.getElementById("imgZoom");
+    var posX = event.offsetX ? (event.offsetX) : event.pageX - img.offsetLeft;
+    var posY = event.offsetY ? (event.offsetY) : event.pageY - img.offsetTop;
+    element.style.backgroundPosition = (-posX * 4) + "px " + (-posY * 4) + "px";
+  
+  }
+  
+  function zoomOut() {
+    var element = document.getElementById("overlay");
+    element.style.display = "none";
+  }
