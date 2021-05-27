d122f122ce9c47706eea4814527a3de3eb9267b3
ColeTruax <82454839+ColeTruax@users.noreply.github.com>
2021-05-27 09:15:27 -0500

    Much stuff

--- Version8.0/user25/config.php
+++ Version8.0/user25/config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'shakopeesabers');
 define('DB_NAME', 'login');
  
 /* Attempt to connect to MySQL database */
