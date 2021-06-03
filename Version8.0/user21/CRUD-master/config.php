2beb2c6e15b7adff9bc04ffb26c3ab802f7dc196
AidanRau <238623@shakopeeschools.org>
2021-06-03 08:28:42 -0500

    all version 7 and 8
    I need these

--- Version8.0/user21/CRUD-master/config.php
+++ Version8.0/user21/CRUD-master/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'shakopeesabers');
-define('DB_NAME', 'login');
+define('DB_NAME', '');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
