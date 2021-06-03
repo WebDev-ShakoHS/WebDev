d99d7d036bec7258e6c4fe91d278cb853b4b4437
webDevUser25 <joe4stgeorge@outlook.com>
2021-06-03 09:37:06 -0500

    Project 5.4 Web Development Final 6/3/21
    Project 5.4 Web Development Final 6/3/21

--- Version8.0/user23/Login/logout.php
+++ Version8.0/user23/Login/logout.php
@@ -1,5 +1,13 @@
 <?php
+// Initialize the session
 session_start();
-unset($_SESSION["id"]);
-unset($_SESSION["name"]);
-header("Location:login.php");
+ 
+// Unset all of the session variables
+$_SESSION = array();
+ 
+// Destroy the session.
+session_destroy();
+ 
+// Redirect to login page
+header("location: login.php");
+exit;
