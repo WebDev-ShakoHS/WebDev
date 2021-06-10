121a1a1e8f60e0d3932f23658f072650ba946f14
nateDake <237742@shakopeeschools.org>
2021-06-10 05:01:39 -0500

    Javascript
    -Filled the boxes
    -gotta make video

--- Version6.0/user02/JS/script.js
+++ Version6.0/user02/JS/script.js
@@ -1,4 +1,39 @@
 //
 function changeText() {
     document.getElementById("textChange").innerHTML="Thanks for liking my Webpage";
+    document.getElementById("textChange").innerHTML="tacobell"
+}
+var space = " ";
+   var pos = 0;
+   var msg = "User 19";
+   
+   function Scroll(){
+   document.title = msg.substring(pos, msg.length) + space +msg.substring(0,pos);
+   
+   pos++;
+   if (pos > msg.length) pos = 0;
+   window.setTimeout("Scroll()", 0);
    }
+   Scroll();
+function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+} 
+function preferedBrowser() {
+    prefer = document.forms[0].browsers.value;
+    alert("You prefer browsing internet with " + prefer);
+}
+function myFunction() {
+    document.getElementById("demo").innerHTML = "You selected some text";
+}
+function myFunction() {
+    alert("You pressed a key inside the input field");
+}
+function confirmInput() {
+    fname = document.forms[0].fname.value;
+    alert("Hello " + fname + "! You will now be redirected to www.w3Schools.com");
+}
+function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
